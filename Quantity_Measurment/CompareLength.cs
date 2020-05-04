bb78b3c0f61870e91fc6dbf7c9fb83c2d178a9b5
trimbakeshwar <hudge.trimbakeshwar@gmail.com>
2020-05-04 12:49:39 +0530

    code for compare feet or inch

---
+++ Quantity_Measurment/CompareLength.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Quantity_Measurment
+{
+    //comparison
+    class CompareLength
+    {
+        public int compareFeetInch(double feet, double inch)
+        {
+            //convert into feet for comparison
+            double inchToFeet = inch / 12;
+
+            //if feet and inchToFeet is same then return 1 else return 0
+            if (feet == inchToFeet)
+            {
+                return 1;
+            }
+            return 0;
+        }
+    }
+}
