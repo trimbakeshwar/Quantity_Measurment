d03d5901e611f2104bcb60d0cf86cacd6d5a8baa
trimbakeshwar <hudge.trimbakeshwar@gmail.com>
2020-05-04 14:14:09 +0530

    uc1.2 add equles method

---
+++ Quantity_Measurment/CheckEquals.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Quantity_Measurment
+{
+    class CheckEquals
+    {
+        public double Feet{set; get;}
+        public CheckEquals(double feet)
+        {
+            this.Feet = feet;
+        }
+        public Boolean equals(object feet)
+        {
+            if(feet==null)
+            {
+                return false;
+            }
+        }
+    }
+}
